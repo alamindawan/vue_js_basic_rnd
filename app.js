1e406d66fc7b7040a2dce17002a15872a5a63304
Md Al Amin Dawan <alamindawancse@gmail.com>
2023-03-19 00:23:14 +0600

    Initial Setup and Data Binding

---
+++ app.js
@@ -0,0 +1,12 @@
+let app = Vue.createApp({
+    data() {
+        return {
+            targetData: {
+                name: "oo og",
+                fb: "https://web.facebook.com/?_rdc=1&_rdr"
+            }
+        }
+    }
+});
+
+app.mount("#app")
